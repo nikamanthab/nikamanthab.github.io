9d86f2146a995c34cfd537b0ba5bd954e2569733
nikamanthab <nitinnikamanth@gmail.com>
2019-07-06 20:47:40 +0530

    Updates

---
+++ precache-manifest.14e6ba79094c4b7eadfba1d1692150ef.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "692e0877a25a7ecff1b3b1bf83698a77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eadf751b6b4577c21a5e",
+    "url": "/static/css/2.fbed5928.chunk.css"
+  },
+  {
+    "revision": "ae096c2af424f10a873a",
+    "url": "/static/css/main.cca6340c.chunk.css"
+  },
+  {
+    "revision": "eadf751b6b4577c21a5e",
+    "url": "/static/js/2.c98e0cec.chunk.js"
+  },
+  {
+    "revision": "ae096c2af424f10a873a",
+    "url": "/static/js/main.3aeaa1b4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "eeadb3a3c7979656d2700b7c2d94bb46",
+    "url": "/static/media/cv.eeadb3a3.pdf"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "721cc5f897438769c59be542af34e243",
+    "url": "/static/media/graduation.721cc5f8.svg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "4a8c4d05b8921ff9cc9a77ef0d7b6ae2",
+    "url": "/static/media/internship.4a8c4d05.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "fe58ba6d5e69a0324a9a20bd9849ae1f",
+    "url": "/static/media/profile.fe58ba6d.jpg"
+  },
+  {
+    "revision": "30ee57fa88ced0f65016e275e7498ab5",
+    "url": "/static/media/school.30ee57fa.svg"
+  },
+  {
+    "revision": "81b01611cbc6e3cb28e203a199290044",
+    "url": "/static/media/wallpaper.81b01611.png"
+  }
+]);
